621504e9094a294cbd30d81bc384aa7ca16e490a
Dario Tranchitella <dario@tranchitella.eu>
2026-04-02 07:39:13 +0200

    feat: age additional printer column for OSArtifact API (#82)
    Signed-off-by: Dario Tranchitella <dario@tranchitella.eu>

--- api/v1alpha2/osartifact_types.go
+++ api/v1alpha2/osartifact_types.go
@@ -250,6 +250,7 @@ type OSArtifactStatus struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.status.phase`
+// +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`,description="Age"
 // +kubebuilder:validation:XValidation:rule="self.spec == oldSelf.spec",message="spec is immutable"
 
 // OSArtifact is the Schema for the osartifacts API
